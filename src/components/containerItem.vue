88dd4bbb03f11acbaf7e28e285dd59a98b45e697
Alpha-Y <mb0420141@gmail.com>
2021-05-01 00:04:19 +0200

    fuck 2

---
+++ src/components/containerItem.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="containerItem">
+    <img :src="img" :alt="name" class="containerItemImg">
+    <div id="itemBG" class="containerItemBg" />
+    <div class="itemDescription containerItemlevel_gradientcut itemRarity4" />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ContainerItem',
+  props: {
+    img: { type: String, required: true },
+    name: { type: String, required: true },
+    rarity: { type: String, required: true }
+  },
+  setup (props) {
+    console.log('props from item:', props)
+
+    const addRarityBackground = () => {
+      const element = document.querySelector('#itemBG')
+      if (element) {
+        console.log('elemt there', props.rarity)
+        switch (props.rarity) {
+          case 'Special':
+            element.classList.add('itemRarity0')
+            break
+          case 'Covert':
+            element.classList.add('itemRarity1')
+            break
+          case 'Classified':
+            element.classList.add('itemRarity2')
+            break
+          case 'Restricted':
+            element.classList.add('itemRarity3')
+            break
+          case 'Mil-Spec':
+            console.log('MILSPEC ADDED?')
+            element.classList.add('itemRarity4')
+            break
+          case 'Industrial':
+            element.classList.add('itemRarity5')
+            break
+          case 'Consumer':
+            element.classList.add('itemRarity6')
+            break
+          default:
+            throw new Error('FUCK')
+        }
+      }
+    }
+
+    addRarityBackground()
+
+    return { }
+  }
+})
+</script>
+
+<style scoped>
+.containerItem {
+  width: 150px;
+  height: 150px;
+  display: inline-block;
+  vertical-align: center;
+  position: relative;
+  margin-left: 1rem;
+}
+
+.containerItemImg {
+  max-width: 100%;
+  width: auto;
+  position: absolute;
+
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+
+  z-index: 2;
+}
+
+.containerItemBg {
+  box-shadow: inset 0px 0px 2px #444;
+  width: 100%;
+  height: 75%;
+
+  position: absolute;
+
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+
+  background: linear-gradient(#555,#888,#aaa,#bbb);
+  opacity: 0.75;
+}
+
+.itemDescription {
+  width: 100%;
+  height: 25%;
+
+  position: absolute;
+
+  bottom: 13.75px;
+  left: 0;
+  right: 0;
+  margin: auto;
+}
+
+.containerItemlevel_gradientcut {
+  height: 5px;
+  -webkit-clip-path: polygon(0% -100px, 100% -100px, 100% 100%, 0% 100%);
+  clip-path: polygon(0% -100px, 100% -100px, 100% 100%, 0% 100%);
+}
+
+.itemRarity6 {
+  background: rgb(176, 195, 217);
+  background: radial-gradient(circle farthest-side,rgb(176, 195, 217),rgb(156, 175, 197));
+  box-shadow: 0px 0px 70px 32px hsla(210, 0%, 30%, 1);
+}
+.itemRarity5 {
+  background: rgb(94, 152, 217);
+  background: radial-gradient(circle farthest-side,rgb(94, 152, 217),rgb(74, 132, 197));
+  box-shadow: 0px 0px 70px 32px hsla(210, 20%, 30%, 1);
+}
+.itemRarity4 {
+  background: rgb(75, 105, 255);
+  background: radial-gradient(circle farthest-side,rgb(75, 105, 255),rgb(55, 85, 235));
+  box-shadow: 0px 0px 70px 32px hsla(235, 50%, 30%, 1);
+}
+.itemRarity3 {
+  background: rgb(136, 71, 255);
+  background: radial-gradient(circle farthest-side,rgb(136, 71, 255),rgb(116, 51, 235));
+  box-shadow: 0px 0px 70px 32px hsla(270, 40%, 25%, 1);
+}
+.itemRarity2 {
+  background: rgb(211, 44, 230);
+  background: radial-gradient(circle farthest-side,rgb(211, 44, 230),rgb(191, 24, 210));
+  box-shadow: 0px 0px 70px 32px hsla(310, 40%, 25%, 1);
+}
+.itemRarity1 {
+  background: rgb(235, 75, 75);
+  background: radial-gradient(circle farthest-side,rgb(235, 75, 75),rgb(215, 55, 55));
+  box-shadow: 0px 0px 70px 32px hsla(0, 40%, 25%, 1);
+}
+.itemRarity0 {
+  background: rgb(255, 215, 0);
+  background: radial-gradient(circle farthest-side,rgb(255, 215, 0),rgb(235, 195, 0));
+  background: radial-gradient(circle farthest-side,rgb(205, 185, 0),rgb(165, 125, 0));
+  box-shadow: 0px 0px 70px 32px rgb(235, 195, 0);
+}
+</style>
--- src/views/CaseOpener.vue
+++ src/views/CaseOpener.vue
@@ -1,43 +1,17 @@
 <template>
   <div id="case-opening">
-    <div>
-      <div class="container animationAreaItems">
-        <div class="row text-center flex-nowrap mx-auto">
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn itemBoxAnW">    </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
-          <div class="itemBoxAn">  </div>
+    <div class="container-fluid">
+      <div v-show="data.openingContainer" class="container animationAreaItems mx-2">
+        <div id="containerItemDiv" class="row g-0 text-center flex-nowrap mx-auto">
+          <ContainerItem
+            v-for="(item, index) in containerItems"
+            :key="item.name + index"
+            :img="'https://steamcommunity-a.akamaihd.net/economy/image/' + item['conditions']['Factory New'].icon_url"
+            :name="item.name"
+            :rarity="item.rarity" />
         </div>
       </div>
-      <button class="btn" id="openContainer" @click="unbox(data.container.content)">Open Prisma</button>
+      <button class="btn btn-info" id="openContainer" @click="unbox(container.content)">Open Prisma</button>
     </div>
 
     <div>
@@ -53,7 +27,8 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from 'vue'
+import { defineComponent, reactive, Ref, ref } from 'vue'
+import ContainerItem from '@/components/containerItem.vue'
 import caseData from '@/testData/cases.json'
 
 export default defineComponent({
@@ -64,24 +39,69 @@ export default defineComponent({
       required: true
     }
   },
+  components: {
+    ContainerItem
+  },
+
   setup (props) {
     console.log(props.slug)
 
-    const getContainer = (containerName: string) => {
+    const randomNumber = (min: number, max: number) => { // min and max included
+      return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+
+    const getContainer = (containerName: string): Record<string, any> => {
       for (let i = 0; i < caseData.length; i++) {
-        if (containerName === caseData[i].name) {
+        if (caseData[i] && containerName === caseData[i].name) {
           return caseData[i]
         }
       }
+      throw new Error('CASE NOT FOUND')
     }
 
+    const getRarity = () => {
+      const rarity = randomNumber(1, 10000000)
+
+      const special_chance = 25575
+      const covert_chance = special_chance + 63939
+      const classified_chance = covert_chance + 319693
+      const restricted_chance = classified_chance + 1598465
+
+      if (rarity <= special_chance) return 0 // 'Special'
+      if (rarity <= covert_chance) return 1 // 'Covert'
+      if (rarity <= classified_chance) return 2 // 'Classified'
+      if (rarity <= restricted_chance) return 3 // 'Restricted'
+      else return 4 // 'Mil-Spec'
+    }
+
+    const createContainerItems = (containerContent: any[][], amount: number) => {
+      const elements = []
+
+      for (let i = 0; i < amount; i++) {
+        const rarity = getRarity()
+        const possibleSkins = containerContent[rarity]
+        const skin = possibleSkins[(randomNumber(1, possibleSkins.length) - 1)]
+        elements.push(skin)
+      }
+
+      console.log('full container: ', elements)
+      return elements
+    }
+
+    const container = getContainer(props.slug)
+
+    const openedSkins: Ref<any[]> = ref([])
+    const containerItems: Ref<any[]> = ref([])
+
+    containerItems.value = createContainerItems(container.content, 50)
+
     const data = reactive({
+      openingContainer: false,
+
       totalCasesOpened: 0,
       spent: 0,
       profit: 0,
-      openedSkins: [{}],
-      unboxedSkin: {},
-      container: getContainer(props.slug)
+      unboxedSkin: {}
     })
 
     const unbox = (containerContent: any[][]) => {
@@ -124,9 +144,13 @@ export default defineComponent({
       if (unboxedSkin.rarity === 'Covert' || !unboxedSkin.rarity) {
         console.warn(unboxedSkin)
       }
+
+      data.openingContainer = true
+      containerItems.value = createContainerItems(containerContent, 50)
+
       startRoll()
 
-      data.openedSkins.push(unboxedSkin)
+      openedSkins.value.push(unboxedSkin)
       data.unboxedSkin = unboxedSkin
       data.totalCasesOpened++
       return { unboxedSkin }
@@ -139,36 +163,13 @@ export default defineComponent({
         openCaseBtn.classList.add('disabled')
       }
 
-      var lineArrays = ['2985px', '2995px', '3005px', '3015px', '3025px', '3035px', '3045px', '3055px', '3065px', '3075px', '3085px', '3095px', '3100px']
+      var lineArrays = ['-2985px', '-2995px', '-3005px', '-3015px', '-3025px', '-3035px', '-3045px', '-3055px', '-3065px', '-3075px', '-3085px', '-3095px', '-3100px']
 
       var landLine = lineArrays[Math.floor(Math.random() * lineArrays.length)]
       console.log(landLine)
 
-      const firstSlot = document.querySelector('.itemBoxAn')
-      if (firstSlot) {
-        firstSlot.animate(
-          { right: landLine },
-          {
-            duration: 12000,
-            easing: 'ease-in-out'
-          }
-        )
-      }
-    }
-
-    const getRarity = () => {
-      const rarity = randomNumber(1, 10000000)
-
-      const special_chance = 25575
-      const covert_chance = special_chance + 63939
-      const classified_chance = covert_chance + 319693
-      const restricted_chance = classified_chance + 1598465
-
-      if (rarity <= special_chance) return 0 // 'Special'
-      if (rarity <= covert_chance) return 1 // 'Covert'
-      if (rarity <= classified_chance) return 2 // 'Classified'
-      if (rarity <= restricted_chance) return 3 // 'Restricted'
-      else return 4 // 'Mil-Spec'
+      const boxx = document.querySelector('#containerItemDiv')
+      if (boxx) (boxx as HTMLElement).style.left = landLine
     }
 
     const getCondition = (float: number): string => {
@@ -190,49 +191,25 @@ export default defineComponent({
       return Math.random() * (maxFloat - minFloat) + minFloat
     }
 
-    const randomNumber = (min: number, max: number) => { // min and max included
-      return Math.floor(Math.random() * (max - min + 1) + min)
-    }
-
-    return { data, unbox }
+    return { data, unbox, container, openedSkins, containerItems }
   }
 })
 </script>
 
 <style scoped>
-.itemBoxAn {
-  border-radius: 0px !important;
-  border: none !important;
-  margin-left: 3px;
-  margin-top: 5px;
-  width: 150px;
-  height: 165px;
-  background: cyan !important;
-  position: relative;
-}
-
-.itemBoxAnW {
-  border-radius: 0px !important;
-  border: none !important;
-  margin-left: 3px;
-  margin-top: 5px;
-  width: 150px;
-  height: 165px;
-  position: relative;
-  background: black !important;
+#containerItemDiv {
+  position: absolute;
+  left: 0;
+  width: auto;
+  height: 100%;
+  transition: left 7s;
 }
 
 .animationAreaItems {
-  width: 100%;
-  min-height: 180px;
-  max-height: 180px;
-  height: 180px !important;
+  height: 150px;
   position: relative;
   text-align: center;
-  display: inline-block;
   overflow: hidden;
-  white-space: nowrap;
-  border: 2px solid red;
 }
 
 .animationAreaItems:before {
